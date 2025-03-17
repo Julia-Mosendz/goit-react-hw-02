d5dea9bd89b55e6c4841f7e524f14bdbd703a9cb
Julia Mosendz <juliadibrova1@gmail.com>
2025-03-17 12:42:29 +0200

    completed storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Description from "./components/Description/Description";
 import Feedback from "./components/Feedback/Feedback";
 import Options from "./components/Options/Options";
 
 function App() {
-  const [feedback, setFeedback] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
+  const [feedback, setFeedback] = useState(() => {
+    const savedFeedback = window.localStorage.getItem("saved-feedback");
+    if (!savedFeedback) {
+      return {
+        good: 0,
+        neutral: 0,
+        bad: 0,
+      };
+    }
+    return JSON.parse(savedFeedback);
   });
+
+  useEffect(() => {
+    window.localStorage.setItem("saved-feedback", JSON.stringify(feedback));
+  }, [feedback]);
 
   const updateFeedback = (feedbackType) => {
     setFeedback((prevFeedback) => {
